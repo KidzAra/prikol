37adf3ee2bc21d9ab09766ca64dc71d2ef63b4bb
Егор Лихачев <95580520+KidzAra@users.noreply.github.com>
2025-09-29 19:46:32 +0500

    задача на шифрование по ключу

---
+++ encryption.py
@@ -0,0 +1,17 @@
+import random
+
+def encrypt(text: str, key: int) -> str:
+    return ''.join(chr((ord(ch) + key) % 0x110000) for ch in text)
+
+def decrypt(text: str, key: int) -> str:
+    return ''.join(chr((ord(ch) - key) % 0x110000) for ch in text)
+
+# пример
+msg = str("Hello, World!")
+key = int(random.randint(1, 100))
+enc = encrypt(msg, key)
+dec = decrypt(enc, key)
+
+print("Зашифрованное: \n", enc)
+print("Расшифрованное:\n", dec)
+print("Ключ:\n ", key)
