6a6d4082e305f5a8d4f11c5f297a1cdd31ffe80d
Егор Лихачев <95580520+KidzAra@users.noreply.github.com>
2025-10-10 09:43:17 +0500

    исправление задачи с фильтрацией списка

--- 1000.3&8.py
+++ 1000.3&8.py
@@ -1,10 +1,17 @@
 countsList = list(range(1,1000))
 
+#def listFiltrer(listToFilter):
+#    finalList = []
+#    for i in listToFilter:
+#        if i % 10 != 3 and i % 10 != 8:
+#            finalList.append(i)
+#    return finalList
+
 def listFiltrer(listToFilter):
-    finalList = []
+    filteredList = []
     for i in listToFilter:
-        if i % 10 != 3 and i % 10 != 8:
-            finalList.append(i)
-    return finalList
+        if '3' not in str(i) and '8' not in str(i):
+            filteredList.append(i)
+    return filteredList
 
 print(listFiltrer(countsList))
